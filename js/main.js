3244fb368c4d5ed110b69830aadb184a9c9cd520
prgnl <alexfilinsky@gmail.com>
2020-10-26 15:33:14 +0100

    haar knippen

--- js/main.js
+++ js/main.js
@@ -11,3 +11,38 @@ typeText = (textToBeTyped)=>{
 }
 
 typeText(Array.from("Dit ben jij Ahmed, 12 jaar oud. Je bent  als een vluchteling naar Nederland gekomen en wilt iets doen voor de Nederlanders als dankbaarheid."));
+
+
+document.getElementsByClassName("image container__items__list__item__scissors--image")[0].style.height = "65rem";
+
+
+function onDragStart(event) {
+  event.target.style.backgroundColor = "#1d2d50";
+  event.dataTransfer.setData("text/plain", event.target.id);
+
+}
+
+function onDragEnd(event) {
+  event.target.style.backgroundColor = "#e11d74";
+
+}
+
+function onDragOver(event) {
+  event.preventDefault();
+
+}
+
+function onDragLeave(event) {
+  document.getElementsByClassName("container__charachter")[0].style.backgroundColor = "#252a34";
+
+}
+
+function onDrop(event) {
+  document.getElementById(event.dataTransfer.getData("text/plain")).style.display="#252a34";
+  event.target.style.backgroundColor = "#252a34";
+  let id = event.dataTransfer.getData("text/plain");
+  console.log(id);
+  if(id === "js--scissors") {
+    document.getElementsByClassName("image container__items__list__item__scissors--image")[0].style.height = "30rem";
+  }
+}
