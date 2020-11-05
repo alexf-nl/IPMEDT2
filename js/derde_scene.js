c08d95d824391477abe556e4892387cab4b0ddd6
teresa113697 <47192822+teresa113697@users.noreply.github.com>
2020-11-05 19:09:57 +0100

    5de scene toevoeging interactie

--- js/derde_scene.js
+++ js/derde_scene.js
@@ -1,7 +1,13 @@
 const eersteMetinghaarResultaat = document.getElementsByClassName("section--container--tekstKapper-header--content")[0];
 const eersteMetinghaar = document.getElementById("js--button--eersteMeting");
+const buttonNaarKapper = document.getElementById("js--button--naarKapper");
 
   eersteMetinghaar.onclick = (event) =>  {
     eersteMetinghaarResultaat.style.display = "block";
   }
+
+  buttonNaarKapper = onclick = (event) => {
+    
+  }
+  
   
