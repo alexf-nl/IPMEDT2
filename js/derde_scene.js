08b5eaf8617063c05f08191a7940f25c3e6ae74e
teresa113697 <47192822+teresa113697@users.noreply.github.com>
2020-11-06 01:43:21 +0100

    5e scene klaar

--- js/derde_scene.js
+++ js/derde_scene.js
@@ -1,13 +1,28 @@
 const eersteMetinghaarResultaat = document.getElementsByClassName("section--container--tekstKapper-header--content")[0];
 const eersteMetinghaar = document.getElementById("js--button--eersteMeting");
-const buttonNaarKapper = document.getElementById("js--button--naarKapper");
+const liniaal = document.getElementById("js--liniaal");
+const lengte = document.getElementById("js--lengte");
+const buttonMeten = document.getElementById("js--button--meten");
+
+
+if(location.pathname == "/derde_scene.html") {
+
+  buttonMeten.onclick = (event) => {
+    const laatsteHaarMeten = document.getElementById("js--laatste--haar--meten");
+
+    liniaal.style.height = "100%";
+    lengte.style.display = "block";
+    laatsteHaarMeten.innerHTML = "<p>Je haar is nu lang genoeg! Wil je nu naar de kapper gaan en je haar laten knippen om het te doneren?</p>";
+    laatsteHaarMeten.style.backgroundColor = "var(--text-background-color)";
+  }
+} 
+
+
+
 
   eersteMetinghaar.onclick = (event) =>  {
     eersteMetinghaarResultaat.style.display = "block";
   }
 
-  buttonNaarKapper = onclick = (event) => {
-    
-  }
-  
-  
+
+
