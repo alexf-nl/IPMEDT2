0bee61d511a9bfc6686b14535087a524af1e6080
prgnl <alexfilinsky@gmail.com>
2020-11-06 02:05:46 +0100

    --

--- js/derde_scene.js
+++ js/derde_scene.js
@@ -4,15 +4,29 @@ const liniaal = document.getElementById("js--liniaal");
 const lengte = document.getElementById("js--lengte");
 const buttonMeten = document.getElementById("js--button--meten");
 const laatsteHaarMeten = document.getElementById("js--laatste--haar--meten");
+const progressBar = document.getElementById('js--progressbar');
+let progressBarValue = 0;
+const progressBarIncrease = 5;
+const progressBarPercentage = document.getElementById('js--progressPercentage');
+
+function progressBarFunctie(progressBarValue) {
+  progressBar.setAttribute('value', progressBarValue);
+  document.getElementById('js--scissors1').style.setProperty('--move', progressBarValue + '%');
+  console.log(progressBarValue);
+  progressBarPercentage.innerHTML = progressBarValue + "%";
+  progressBarPercentage.style.setProperty('--move', progressBarValue + "%");
+}
 
 
 if(location.pathname == "/derde_scene.html") {
-  console.log('hey');
+  progressBarFunctie(35);
   eersteMetinghaar.onclick = (event) =>  {
     eersteMetinghaarResultaat.style.display = "block";
   }
 
 } else if(location.pathname == "/vijfde_scene.html") {
+  progressBarFunctie(70);
+
   buttonMeten.onclick = (event) => {
   liniaal.style.height = "100%";
   lengte.style.display = "block";
