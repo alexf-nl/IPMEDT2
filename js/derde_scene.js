afb684cc4a85d9935482b555b0c65cc6373766ee
prgnl <alexfilinsky@gmail.com>
2020-11-06 01:56:54 +0100

    --

--- js/derde_scene.js
+++ js/derde_scene.js
@@ -3,23 +3,21 @@ const eersteMetinghaar = document.getElementById("js--button--eersteMeting");
 const liniaal = document.getElementById("js--liniaal");
 const lengte = document.getElementById("js--lengte");
 const buttonMeten = document.getElementById("js--button--meten");
+const laatsteHaarMeten = document.getElementById("js--laatste--haar--meten");
 
 
 if(location.pathname == "/derde_scene.html") {
-
-  buttonMeten.onclick = (event) => {
-    const laatsteHaarMeten = document.getElementById("js--laatste--haar--meten");
-
-    liniaal.style.height = "100%";
-    lengte.style.display = "block";
-    laatsteHaarMeten.innerHTML = "<p>Je haar is nu lang genoeg! Wil je nu naar de kapper gaan en je haar laten knippen om het te doneren?</p>";
-    laatsteHaarMeten.style.backgroundColor = "var(--text-background-color)";
-  }
-}
-
-
-
-
+  console.log('hey');
   eersteMetinghaar.onclick = (event) =>  {
     eersteMetinghaarResultaat.style.display = "block";
   }
+
+} else if(location.pathname == "/vijfde_scene.html") {
+  buttonMeten.onclick = (event) => {
+  liniaal.style.height = "100%";
+  lengte.style.display = "block";
+   laatsteHaarMeten.innerHTML = "<p>Je haar is nu lang genoeg! Wil je nu naar de kapper gaan en je haar laten knippen om het te doneren?</p>";
+   laatsteHaarMeten.style.backgroundColor = "var(--text-background-color)";
+  }
+
+}
